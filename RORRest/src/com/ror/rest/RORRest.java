a46f66c00650d46042efba22ae1019e00f02b44c
574384 <574384@PC267095.cts.com>
2018-04-20 19:05:32 +0530

    RORDAO depedency added and Rest Modified.

--- RORRest/src/com/ror/rest/RORRest.java
+++ RORRest/src/com/ror/rest/RORRest.java
@@ -1,8 +1,21 @@
 package com.ror.rest;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import com.ror.dao.RORDAO;
+import com.ror.dao.impl.RORDAOImpl;
 
 @Path("rest")
 public class RORRest {
+	
+
+	@GET
+	@Path("/sample")
+	public String userAvailabilityService() {
+		RORDAO rorDAO = new RORDAOImpl();
+		return rorDAO.sampleDAOCheck();
+	}
+	
 
 }
